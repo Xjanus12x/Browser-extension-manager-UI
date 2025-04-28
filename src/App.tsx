3acfac91c8dca487dd60c97eb16b5253c74599da
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-28 19:04:17 +0800

    feat: add Header and ExtensionGrid components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import Header from "./component/Header";
+import ExtensionGrid from "./component/ExtensionGrid";
+
 export default function App() {
-  return <></>;
+  return (
+    <main className="p-4 space-y-8 xl:px-0 bg-neutral-200 wrapper min-h-dvh md:space-y-10">
+      <Header />
+      <ExtensionGrid />
+    </main>
+  );
 }
