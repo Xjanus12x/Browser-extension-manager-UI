01ab1e0eb52bd2c199b3e26018edb9bfe7cd43b7
xjanus12x <albertovillacarlos07@gmail.com>
2025-04-28 19:00:55 +0800

    feat: create ExtensionGrid component with filtering and dynamic list rendering

---
+++ src/component/ExtensionGrid.tsx
@@ -0,0 +1,64 @@
+import { Button } from "./Button";
+import { useExtensionStore } from "../store/useExtensionStore";
+import ExtensionCard from "./ExtensionCard";
+import chitoge from "../assets/images/chitoge.png";
+import { cn } from "../utils/cn";
+
+const activeFilterClass =
+  "bg-red-700 text-neutral-0 hover:bg-red-500 focus-visible:bg-red-500";
+
+export default function ExtensionGrid() {
+  const currentFilter = useExtensionStore((state) => state.currentFilter);
+  const extensionList = useExtensionStore((state) => state.extensionList);
+  const filterExtensionList = useExtensionStore(
+    (state) => state.filterExtensionList
+  );
+
+  return (
+    <section className="col-start-2 space-y-8 md:space-y-4">
+      <header className="space-y-4 text-center sm:flex sm:items-center sm:justify-between">
+        <h1
+          className="text-2xl font-bold md:text-3xl text-neutral-900"
+          id="extension-list"
+        >
+          Extensions List
+        </h1>
+        <nav className="space-x-2" aria-label="Extension Filters">
+          <Button
+            className={cn(currentFilter === "all" && activeFilterClass)}
+            label="All"
+            onClick={() => filterExtensionList("all")}
+          />
+          <Button
+            className={cn(currentFilter === "active" && activeFilterClass)}
+            label="Active"
+            onClick={() => filterExtensionList("active")}
+          />
+          <Button
+            className={cn(currentFilter === "inactive" && activeFilterClass)}
+            label="Inactive"
+            onClick={() => filterExtensionList("inactive")}
+          />
+        </nav>
+      </header>
+
+      <section
+        className="grid gap-2.5 extensions-list"
+        aria-labelledby="extension-list"
+      >
+        {extensionList && extensionList.length > 0 ? (
+          extensionList.map((extension, i) => (
+            <ExtensionCard {...extension} key={i} />
+          ))
+        ) : (
+          <ExtensionCard
+            logo={chitoge}
+            name="BAKKA! NOTHING IS LEFT!"
+            description="You've removed all extensions. Time to add some back!"
+            isActive={false}
+          />
+        )}
+      </section>
+    </section>
+  );
+}
