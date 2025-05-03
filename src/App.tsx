518cc0136b36d88a297604c12ae26fc1d9277a9d
xjanus12x <albertovillacarlos07@gmail.com>
2025-05-03 20:15:44 +0800

    feat: update background to theme gradient in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ExtensionGrid from "./component/ExtensionGrid";
 
 export default function App() {
   return (
-    <main className="p-4 space-y-8 xl:px-0 bg-neutral-200 wrapper min-h-dvh md:space-y-10">
+    <main className="p-4 space-y-8 xl:px-0 wrapper min-h-dvh md:space-y-10 bg-theme-gradient">
       <Header />
       <ExtensionGrid />
     </main>
