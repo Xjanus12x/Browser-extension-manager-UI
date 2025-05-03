98f5a86e0adc1ea86ae412b45ed453e06d6a6c98
xjanus12x <albertovillacarlos07@gmail.com>
2025-05-03 20:16:51 +0800

    feat: enhance ExtensionGrid with active/inactive filter styles

--- src/component/ExtensionGrid.tsx
+++ src/component/ExtensionGrid.tsx
@@ -2,14 +2,18 @@ import { Button } from "./Button";
 import { useExtensionStore } from "../store/useExtensionStore";
 import ExtensionCard from "./ExtensionCard";
 import chitoge from "../assets/images/chitoge.png";
-import { cn } from "../utils/cn";
 
-const activeFilterClass =
-  "bg-red-700 text-neutral-0 hover:bg-red-500 focus-visible:bg-red-500";
+const activeFilterBtnClass =
+  "bg-filter-btn-active-background text-filter-btn-active-foreground focus-visible:outline-filter-btn-ring focus-visible:outline-offset-1 focus-visible:border-filter-btn-focus-border border-filter-btn-active-border hover:bg-filter-btn-active-hover-background";
+
+const inactiveFilterBtnClass =
+  "focus-visible:outline-filter-btn-ring focus-visible:bg-filter-btn-focus-background focus-visible:border-filter-btn-focus-border text-filter-btn-inactive-foreground  bg-filter-btn-inactive-background border-filter-btn-inactive-border focus-visible:outline-offset-2 hover:bg-filter-btn-inactive-hover-background";
 
 export default function ExtensionGrid() {
   const currentFilter = useExtensionStore((state) => state.currentFilter);
-  const extensionList = useExtensionStore((state) => state.extensionList);
+  const filteredExtensionList = useExtensionStore(
+    (state) => state.filteredExtensionList
+  );
   const filterExtensionList = useExtensionStore(
     (state) => state.filterExtensionList
   );
@@ -18,24 +22,37 @@ export default function ExtensionGrid() {
     <section className="col-start-2 space-y-8 md:space-y-4">
       <header className="space-y-4 text-center sm:flex sm:items-center sm:justify-between">
         <h1
-          className="text-2xl font-bold md:text-3xl text-neutral-900"
+          className="text-2xl font-bold md:text-3xl text-extension-grid-title"
           id="extension-list"
         >
           Extensions List
         </h1>
+
         <nav className="space-x-2" aria-label="Extension Filters">
           <Button
-            className={cn(currentFilter === "all" && activeFilterClass)}
+            className={
+              currentFilter === "all"
+                ? activeFilterBtnClass
+                : inactiveFilterBtnClass
+            }
             label="All"
             onClick={() => filterExtensionList("all")}
           />
           <Button
-            className={cn(currentFilter === "active" && activeFilterClass)}
+            className={
+              currentFilter === "active"
+                ? activeFilterBtnClass
+                : inactiveFilterBtnClass
+            }
             label="Active"
             onClick={() => filterExtensionList("active")}
           />
           <Button
-            className={cn(currentFilter === "inactive" && activeFilterClass)}
+            className={
+              currentFilter === "inactive"
+                ? activeFilterBtnClass
+                : inactiveFilterBtnClass
+            }
             label="Inactive"
             onClick={() => filterExtensionList("inactive")}
           />
@@ -46,8 +63,8 @@ export default function ExtensionGrid() {
         className="grid gap-2.5 extensions-list"
         aria-labelledby="extension-list"
       >
-        {extensionList && extensionList.length > 0 ? (
-          extensionList.map((extension) => (
+        {filteredExtensionList && filteredExtensionList.length > 0 ? (
+          filteredExtensionList.map((extension) => (
             <ExtensionCard {...extension} key={extension.name} />
           ))
         ) : (
